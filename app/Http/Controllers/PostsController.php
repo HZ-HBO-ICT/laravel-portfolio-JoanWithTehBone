90e08ffb58b532efaa445d318aec130ffd1c04ce
JoanWithTehBone <70522505+JoanWithTehBone@users.noreply.github.com>
2022-02-10 12:36:19 +0100

    Fixed the commenting errors given in Github

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,9 @@ use App\Models\Post;
 
 class PostsController extends Controller
 {
+    /**
+     * @returns view of the of the Posts Pages
+     */
     public function show($slug)
     {
         $post = Post::where('slug', $slug)->firstOrFail();
